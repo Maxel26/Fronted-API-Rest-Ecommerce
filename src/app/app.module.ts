5691f0fa170bf10fcdfc845a15bf05dde848f3c9
unknown <Maxel26>
2023-07-12 21:46:47 -0500

    [+]index.html : CDN Boostrap [+] despedida.component : Directiva ngFor para iterar una lista y desplegar los datos en vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Saludo } from './components/saludo.component';
+import { DespedidaComponent } from './components/despedida/despedida.component';
 
 @NgModule({
   declarations: [
-    AppComponent,Saludo
+    AppComponent,Saludo, DespedidaComponent
   ],
   imports: [
     BrowserModule
