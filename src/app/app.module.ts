77e11881066598d4e5307d3ed11c1a377e943554
unknown <Maxel26>
2023-07-13 18:50:49 -0500

    [+] Borramos todos los componentes menos app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Saludo } from './components/saludo.component';
-import { DespedidaComponent } from './components/despedida/despedida.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,Saludo, DespedidaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
